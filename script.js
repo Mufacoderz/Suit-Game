6f3b4b004df737d3d3a636d6b6a2494558e526a2
Muhammad <mf9555759@gmail.com>
2025-06-03 19:09:25 +0800

    90%

--- script.js
+++ script.js
@@ -1,22 +1,87 @@
 const gunting = document.getElementById('gunting')
 const batu = document.getElementById('batu')
-const kertass = document.getElementById('kertas')
+const kertas = document.getElementById('kertas')
 const playerChoice = document.getElementById('player-choice-img')
+const computerChoice = document.getElementById('computer-choice-img')
+const teksPlayerScore = document.getElementById('player-score')
+const teksComputerScore = document.getElementById('computer-score')
+const boardMenang = document.querySelector('.board-menang')
+const boardKalah = document.querySelector('.board-kalah')
 
-// gunting.addEventListener('click', () => {
-//     playerChoice.setAttribute('src', `img/gunting.png`)
-// })
-// batu.addEventListener('click', () => {
-//     playerChoice.setAttribute('src', `img/batu.png`)
-// })
-// kertas.addEventListener('click', () => {
-//     playerChoice.setAttribute('src', `img/kertas.png`)
-// })
+let playerScore = 0
+let computerScore = 0
 
-function showPlayerChoice(choice){
-    playerChoice.setAttribute('src', `img/${choice}.png`)
+function getComputerChoice(){
+    let botComputer = Math.random()
+    if(botComputer < 0.33) {
+        return 'gunting'
+    }
+    else if(botComputer >= 0.33 && botComputer <0.77){
+        return 'batu'
+    }
+    else{
+        return 'kertas'
+    }
 }
 
-gunting.addEventListener('click', ()=> showPlayerChoice('gunting'))
-batu.addEventListener('click', ()=> showPlayerChoice('batu'))
-kertas.addEventListener('click', ()=> showPlayerChoice('kertas'))
+//atur rule game
+function getResult(botComputer, player){
+    if(player == botComputer) return 'Seri';
+    if(player == 'gunting') return (botComputer == 'batu') ? 'Kalah' : 'Menang';
+    if(player == 'batu') return (botComputer == 'kertas') ? 'Kalah' : 'Menang';
+    if(player == 'kertas') return (botComputer == 'gunting') ? 'Kalah' : 'Menang';
+}
+
+//reset gambar pilihan
+function resetImgChoice(){
+    playerChoice.setAttribute('src', 'img/tanya.png')
+    computerChoice.setAttribute('src', 'img/tanya.png')
+}
+
+
+
+function playGame(choice){
+    // namppilkan img pilihan player
+    playerChoice.setAttribute('src', `img/${choice}.png`)
+    
+    //ambil pilihan kompuetr
+    const bot = getComputerChoice()
+    computerChoice.setAttribute('src', `img/${bot}.png`)
+
+    //resultnya
+    const result = getResult(bot, choice)
+  
+    if(result == 'Menang'){
+        playerScore++
+        teksPlayerScore.textContent =`Player : ${playerScore}`
+    }
+    if(result == 'Kalah'){
+        computerScore++
+        teksComputerScore.textContent = `Computer : ${computerScore}`
+    }
+
+     
+    if(playerScore == 10){
+        boardMenang.classList.remove('hidden')
+    }
+    if(computerScore == 10){
+        boardKalah.classList.remove('hidden')
+    }
+
+
+  
+
+    //reset gambarnya
+    // setTimeout(resetImgChoice, 5000)
+    
+
+}
+
+
+
+gunting.addEventListener('click', ()=> playGame('gunting'))
+batu.addEventListener('click', ()=> playGame('batu'))
+kertas.addEventListener('click', ()=> playGame('kertas'))
+
+
+
